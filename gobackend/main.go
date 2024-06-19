51581acec69aac22d93e050c1b4fa6920a5e6256
Alt4nsuh <112462898+Alt4nsuh@users.noreply.github.com>
2024-06-19 16:00:51 +0800

    models , bai uusgeh nemsen

--- gobackend/main.go
+++ gobackend/main.go
@@ -1,24 +1,94 @@
 package main
 
 import (
+	"gobackend/models"
+	"gobackend/storage"
 	"log"
-	"your_module/models" // adjust the import path according to your module name
+	"os"
 
-	"gorm.io/driver/sqlite"
+	"github.com/gofiber/fiber/v2"
+	"github.com/joho/godotenv"
 	"gorm.io/gorm"
 )
 
-func main() {
-	// Open a connection to the database
-	db, err := gorm.Open(sqlite.Open("test.db"), &gorm.Config{})
+type Repository struct {
+	DB *gorm.DB
+}
+
+func (r *Repository) CreateBaiguullaga(c *fiber.Ctx) error {
+	bai := models.Baiguullaga{}
+	err := c.BodyParser(&bai)
 	if err != nil {
-		log.Fatalf("Failed to connect to the database: %v", err)
+		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{
+			"message": "Failed to parse request body",
+		})
+	}
+	err = r.DB.Create(&bai).Error
+	if err != nil {
+		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
+			"message": "Failed to create baiguullaga",
+		})
+	}
+	return c.JSON(fiber.Map{
+		"Message": "Baiguullaga successfully created",
+		"data":    bai,
+	})
+}
+func (r *Repository) GetBaiguullaga(c *fiber.Ctx) error {
+	bai := []models.Baiguullaga{}
+	err := r.DB.Find(&bai).Error
+	if err != nil {
+		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
+			"message": "Failed to get baiguullaga",
+		})
+	}
+	return c.JSON(fiber.Map{
+		"Message": "Baiguullaga successfully get",
+		"data":    bai,
+	})
+}
+
+func (r *Repository) SetupRouters(app *fiber.App) {
+	api := app.Group("/api")
+	api.Post("/createBai", r.CreateBaiguullaga)
+	api.Post("/getBai", r.GetBaiguullaga)
+
+}
+
+func main() {
+	err := godotenv.Load(".env")
+	if err != nil {
+		log.Fatal(err)
 	}
 
-	// Migrate the schemas
-	if err := models.MigrateAll(db); err != nil {
+	config := &storage.Config{
+		Host:     os.Getenv("DB_HOST"),
+		Port:     os.Getenv("DB_PORT"),
+		Password: os.Getenv("DB_PASSWORD"),
+		User:     os.Getenv("DB_USER"),
+		DBName:   os.Getenv("DB_DBNAME"),
+		SSLMode:  os.Getenv("DB_SSLMODE"),
+	}
+
+	db, err := storage.NewConnection(config)
+	if err != nil {
+		log.Fatal("could not load the database")
+	}
+
+	if err = models.MigrateAll(db); err != nil {
 		log.Fatalf("Failed to migrate database: %v", err)
 	}
+	r := Repository{
+		DB: db,
+	}
 
-	log.Println("Database migration completed successfully.")
+	app := fiber.New()
+
+	// app.Use(cors.New(cors.Config{
+	// 	AllowCredentials: true,
+	// 	AllowOrigins:     "http://localhost:3000",
+	// }))
+
+	r.SetupRouters(app)
+	log.Fatal(app.Listen(":8080"))
 }
