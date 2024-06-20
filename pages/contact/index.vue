4177e439e04b5075005e20eb867d5733a598ea13
davaasambuu <davaasambuu@gerege.mn>
2024-06-20 17:16:46 +0800

    registration form arai goy bolson

--- pages/contact/index.vue
+++ pages/contact/index.vue
@@ -3,35 +3,65 @@
   <div>
     <Header/>
   </div>
-  <div class="bg-sky-50 flex">
-    <div class="mx-32 my-32 bg-white rounded-lg w-2/4 flex  shadow-lg border">
-      <UForm :schema="schema" :state="state" class="space-y-4" @submit="onSubmit">
-       <!-- name of org -->
-         <UFormGroup  name="org" class="my-12 mx-12">
-          <span class="text-slate-900 ">Байгууллагын нэр :</span>
-           <UInput v-model="state.org" color="sky"/>
-         </UFormGroup>
- 
-
-         <!-- about by org -->
-         <p>Байгууллагын тухай :</p>
-         <UTextarea v-model="value" color="sky"/>
-         <!-- zurag oruulah heseg -->
-         <p>Байгууллагын зураг :</p>
-         <UInput type="file" size="sm" icon="i-heroicons-folder" color="sky"/>
-     
-         <UFormGroup label="Утасны дугаар :" name="phone">
-           <UInput v-model="state.phone" type="phone" color="sky"/>
-         </UFormGroup>
-     
-         <UButton type="submit">Next</UButton>
-       </UForm>
+ <!-- Body -->
+  <div class="bg-sky-50 h-full w-full flex justify-center">
+   <div class="bg-white border shadow-sm w-2/4 shadow-sky-500 transition scale-90 hover:scale-100 rounded-lg">
+    <p class="font-bold text-2xl text-slate-900 text-center my-6">Registration</p>
+    <div class="mt-6">
+            <UFormGroup  name="org" class="mx-4">
+          <span class="text-slate-900 text-sm">First name *</span>
+           <UInput v-model="state.org" color="sky" placeholder="First name..." variant="outline"/>
+         </UFormGroup>  
     </div>
+    <div class="mt-6">
+            <UFormGroup  name="org" class="mx-4">
+          <span class="text-slate-900 text-sm">First name *</span>
+           <UInput v-model="state.org" color="sky" placeholder="First name..." variant="outline"/>
+         </UFormGroup>  
+    </div>
+    <div class="mt-6">
+            <UFormGroup  name="org" class="mx-4">
+          <span class="text-slate-900 text-sm">First name *</span>
+           <UInput v-model="state.org" color="sky" placeholder="First name..." variant="outline"/>
+         </UFormGroup>  
+    </div>
+    <div class="mt-6">
+            <UFormGroup  name="org" class="mx-4">
+          <span class="text-slate-900 text-sm">First name *</span>
+           <UInput v-model="state.org" color="sky" placeholder="First name..." variant="outline"/>
+         </UFormGroup>  
+    </div>
+    <div class="mt-6 mb-12">
+            <UFormGroup  name="org" class="mx-4">
+          <span class="text-slate-900 text-sm">First name *</span>
+           <UInput v-model="state.org" color="sky" placeholder="First name..." variant="outline"/>
+         </UFormGroup>  
+    </div>
+
+   </div>
   </div>
   <!-- FooterL -->
-  <div>
-    <Footer/>
-  </div>
+  <div class="h-fit mx-12 bg-white border text-wrap flex">
+        
+        <div class="ml-28 my-4 flex ">
+            <p class="text-slate-900">Gersystem сайтад зочилсонд баярлалаа. Та аян замдаа сайн яваарай<Icon name="material-symbols-light:sentiment-satisfied-outline" class="h-6 w-6"/></p>
+        </div>
+        <div class="flex space-x-14">
+            <div class="bg-sky-50 transition ease-in-out hover:scale-110 h-10 w-10 my-6 rounded-xl ml-80  border shadow-xl">
+                <Icon name="ic:baseline-facebook" class="text-sky-500 ml-1 my-2 h-6 w-8"/>
+            </div>
+            <div class="bg-sky-50 h-10 w-10 transition ease-in-out hover:scale-110 my-6 rounded-xl ml-96  border shadow-xl">
+                <Icon name="tabler:brand-instagram" class="text-sky-500 ml-1 my-2 h-6 w-8"/>
+            </div>
+            <div class="bg-sky-50 h-10 transition ease-in-out hover:scale-110 w-10 my-6 rounded-xl ml-96  border shadow-xl">
+                <Icon name="mdi:twitter" class="text-sky-500 ml-1 my-2 h-6 w-8"/>
+            </div>
+            <div class="bg-sky-50 h-10 transition ease-in-out hover:scale-110 w-10 my-6 rounded-xl ml-96  border shadow-xl">
+                <Icon name="material-symbols:mail-outline" class="text-sky-500 ml-1 my-2 h-6 w-8"/>
+            </div>
+            
+        </div>
+    </div>
 </template>
 
 <script lang="ts" setup>
