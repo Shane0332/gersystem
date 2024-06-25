d9bf697d8c3d4867483d331d1259a558d707c936
Alt4nsuh <112462898+Alt4nsuh@users.noreply.github.com>
2024-06-25 15:38:31 +0800

    burtgel

--- components/header.vue
+++ components/header.vue
@@ -1,31 +1,4 @@
 <template>
-<<<<<<< HEAD
-    <div class="bg-sky-50 w-full md:w-full h-100">
-        <div class="h-12  bg-white border  text-stone-900  md:flex lg:80">
-            <div class="flex justify-start space-x-10 ml-32 text-sm ">
-                <a href="#" class="hover:text-sky-500 my-4"><Icon name="ic:baseline-local-phone" class="mr-1 mb-0.5"/>96611510</a>
-                <a href="#" class="hover:text-sky-500 my-4"><Icon name="ic:outline-mail" class="mr-1 mb-0.5"/>Gersystem@gmail.com</a>
-                <a href="https://www.facebook.com/profile.php?id=100066647678479" class="hover:text-sky-500 my-4"><Icon name="ic:baseline-facebook" class="mr-1 mb-0.5"/>Gersystem</a>
-                <a href="https://www.instagram.com/ripperflyy/" class="hover:text-sky-500 my-4"><Icon name="tabler:brand-instagram" class="mr-1 mb-0.5"/>gersystem</a>
-            </div>
-        </div>
-        <div class="bg-white shadow-inner rounded-lg h-24  flex">
-            <div class="w-28 h-28 justify-start ml-28 lg:w-28">
-                <div class="md:shrink-0">
-                    <img src="~/assets/Logo.png" alt="" class="">
-                </div>
-            </div>
-            <p class="text-xl font-bold text-slate-900 my-5 md:text-4xl">Ger System</p>
-            <div class="my-7 mx-20 space-x-11 ml-52 ">
-                <div class="md:w-32 lg:wp">
-                    <NuxtLink to="/home"><Button class="transition ease-in-out  bg-sky-500  hover:scale-110 shadow-sm hover:bg-slate-900  border  h-10 text-sm  w-24 rounded-lg">НҮҮР</Button></NuxtLink>
-                <NuxtLink to="/contact"><Button class="transition ease-in-out  bg-sky-500  hover:scale-110 shadow-2xl hover:bg-slate-900  border  h-10 text-sm  w-28 rounded-lg">ГЭР НЭМЭХ</Button></NuxtLink>
-                <NuxtLink to="/geruud"><Button class="transition ease-in-out  bg-sky-500  hover:scale-110 shadow-2xl hover:bg-slate-900  border h-10 text-sm  w-36 rounded-lg">ГЭР ЗАХИАЛАХ</Button></NuxtLink>
-                <NuxtLink to="/create"><Button class="transition ease-in-out  bg-sky-500  hover:scale-110 shadow-2xl hover:bg-slate-900  border h-10 text-sm  w-36 rounded-lg uppercase">Нэвтрэх</Button></NuxtLink>
-                </div>
-
-            </div>
-=======
 <div class="bg-sky-50 w-full md:w-full h-100">
     <div class="h-12  bg-white border  text-stone-900 mx-12 md:32 lg:80">
         <div class="flex justify-start space-x-10 ml-32 text-sm ">
@@ -33,7 +6,6 @@
             <a href="#" class="hover:text-sky-500 my-4"><Icon name="ic:outline-mail" class="mr-1 mb-0.5"/>Gersystem@gmail.com</a>
             <a href="https://www.facebook.com/profile.php?id=100066647678479" class="hover:text-sky-500 my-4"><Icon name="ic:baseline-facebook" class="mr-1 mb-0.5"/>Gersystem</a>
             <a href="https://www.instagram.com/ripperflyy/" class="hover:text-sky-500 my-4"><Icon name="tabler:brand-instagram" class="mr-1 mb-0.5"/>gersystem</a>
->>>>>>> 3f736168e8ff6e2d59e90c93d72ca61e8e574d92
         </div>
     </div>
     <div class="bg-white shadow-inner rounded-lg h-24 mx-12 flex">
--- pages/geruud/index.vue
+++ pages/geruud/index.vue
@@ -5,39 +5,6 @@
     <div>
       <Header/>
     </div>
-<<<<<<< HEAD
-    <!-- Body heseg -->
-   <div class="bg-sky-50 h-full w-full">
-    <div class="bg-white text-sm h-full border shadow-xl md:text-sm">
-        <p class="text-black ml-12 my-6">Та дараах жуулчны баазуудаас өөрт тохирохыг сонгоод дарна уу!</p>
-        <div class="bg-white border my-6 rounded-lg  flex">
-            <div>
-                <NuxtLink to="/bluesky"><img src="~/assets/ger2.jpg" alt="" class=" mx-6 h-32 w-32 md:h-96 md:w-4/5 my-4 rounded-lg transition hover:scale-110"></NuxtLink>
-            </div>
-            <div>
-                <p class="font-bold text-black hover:text-sky-500  my-4 md:text-4xl  text-2xl">Blue Sky Eco Tourist Camp</p>
-                <div class=" my-2 flex">
-                    <div>
-                        <div class="flex bg-sky-50 h-6 w-18 md:w-8 border rounded-lg">
-                            <p class="text-black text-xs md:text-sm mx-6"><Icon name="material-symbols:location-on-rounded" class="mr-1 mb-0.5 text-sky-500"/>Өв Бат-Өлзий сум</p>
-                        </div>
-                        <div class="bg-sky-50 my-4 h-6 w-36 border rounded-lg">                        
-                            <p class="text-black text-sm mx-4"><Icon name="material-symbols:other-houses" class="mr-1 mb-0.5 text-sky-500"/>Гэрийн тоо : 5</p>
-                        </div>
-                        <div class="bg-sky-50 my-4 h-6 w-36 border rounded-lg">                        
-                            <p class="text-black text-sm mx-4"><Icon name="ic:baseline-signal-cellular-alt" class="mr-1 mb-0.5 text-sky-500"/>Сүлжээ : 4G</p>
-                        </div>
-                        <div class="bg-sky-50 my-4 h-6 w-52 border rounded-lg">                        
-                            <p class="text-black text-sm mx-4"><Icon name="ic:round-follow-the-signs" class="mr-1 mb-0.5 text-sky-500"/>Сумийн төвөөс : 5км</p>
-                        </div>
-                        <div class="bg-sky-50 my-4 h-6 w-36 border rounded-lg">                        
-                            <p class="text-black text-sm mx-4"><Icon name="mdi:food-croissant" class="mr-1 mb-0.5 text-sky-500"/>Өглөөний цай</p>
-                        </div>
-                    </div>
-                    <div class="bg-sky-50  mx-6 h-44 w-64 border">
-                      <NuxtLink to="https://www.google.com/maps/place/Blue+Sky+Eco+Tourist+camp/@46.7927007,101.9381137,1248m/data=!3m1!1e3!4m6!3m5!1s0x5d82cbe9fb7f55bd:0x4bb78c40d5ef1f4c!8m2!3d46.7921446!4d101.9407811!16s%2Fg%2F11kp_99gt2!5m1!1e1?entry=ttu"><img src="~/assets/map.png" alt="" class="rounded-lg "></NuxtLink>
-                    </div>
-=======
   
     <!-- Main content -->
     <div class="bg-sky-50 h-full w-full">
@@ -60,7 +27,6 @@
               <div>
                 <div class="flex bg-sky-50 h-6 w-52 md:w-52 border rounded-lg">
                   <p class="text-black text-sm mx-6"><Icon name="material-symbols:location-on-rounded" class="mr-1 mb-0.5 text-sky-500"/>Өв Бат-Өлзий сум</p>
->>>>>>> 3f736168e8ff6e2d59e90c93d72ca61e8e574d92
                 </div>
                 <div class="bg-sky-50 my-4 h-6 w-36 border rounded-lg">
                   <p class="text-black text-sm mx-4"><Icon name="material-symbols:other-houses" class="mr-1 mb-0.5  text-sky-500"/>Гэрийн тоо : {{ba.b_gertoo}}</p>
@@ -117,45 +83,6 @@
         </div>
       </div>
     </div>
-<<<<<<< HEAD
-   </div>
-   
-<!-- Footer heseg -->
-<div class="bg-sky-50 w-full h-full">
-
-   
-    <div class="h-fit  bg-white border text-wrap flex">
-        
-        <div class="ml-28 my-4 text-xs flex md:text-xs">
-            <p class="text-slate-900">Gersystem сайтад зочилсонд баярлалаа. Та аян замдаа сайн яваарай<Icon name="material-symbols-light:sentiment-satisfied-outline" class="h-6 w-6"/></p>
-        </div>
-        <div class="flex space-x-14">
-            <div class="bg-sky-50 transition ease-in-out hover:scale-110 h-10 w-10 my-6 rounded-xl ml-80  border shadow-xl">
-                <Icon name="ic:baseline-facebook" class="text-sky-500 ml-1 my-2 h-6 w-8"/>
-            </div>
-            <div class="bg-sky-50 h-10 w-10 transition ease-in-out hover:scale-110 my-6 rounded-xl ml-96  border shadow-xl">
-                <Icon name="tabler:brand-instagram" class="text-sky-500 ml-1 my-2 h-6 w-8"/>
-            </div>
-            <div class="bg-sky-50 h-10 transition ease-in-out hover:scale-110 w-10 my-6 rounded-xl ml-96  border shadow-xl">
-                <Icon name="mdi:twitter" class="text-sky-500 ml-1 my-2 h-6 w-8"/>
-            </div>
-            <div class="bg-sky-50 h-10 transition ease-in-out hover:scale-110 w-10 my-6 rounded-xl ml-96  border shadow-xl">
-                <Icon name="material-symbols:mail-outline" class="text-sky-500 ml-1 my-2 h-6 w-8"/>
-            </div>
-            
-        </div>
-    </div>
-    </div>
-   
-</template>
-
-<script lang="ts" setup>
-definePageMeta({
-  layout: "default"
-})
-</script>
-
-=======
   
     <!-- Footer section -->
     <Footer/>
@@ -183,4 +110,3 @@ definePageMeta({
   /* Add any scoped styles if needed */
   </style>
   
->>>>>>> 3f736168e8ff6e2d59e90c93d72ca61e8e574d92
